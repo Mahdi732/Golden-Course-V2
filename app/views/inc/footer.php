c2bcd974e602eefb95b4e79a4ed11389b6612bc0
Mahdi732 <ey449751@gmail.com>
2025-02-07 15:44:56 +0100

    Add header and footer templates with styles and scripts

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -67,3 +67,19 @@
             </div>
         </div>
 </footer>
+
+<a href="#" class="btn btn-lg btn-primary btn-lg-square rounded back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo URLROOT; ?>/lib/wow/wow.min.js"></script>
+<script src="<?php echo URLROOT; ?>/lib/easing/easing.min.js"></script>
+<script src="<?php echo URLROOT; ?>/lib/waypoints/waypoints.min.js"></script>
+<script src="<?php echo URLROOT; ?>/lib/counterup/counterup.min.js"></script>
+<script src="<?php echo URLROOT; ?>/lib/owlcarousel/owl.carousel.min.js"></script>
+
+<!-- Template Javascript -->
+<script src="<?php echo URLROOT; ?>/js/main.js"></script>
+</body>
+
+</html>
